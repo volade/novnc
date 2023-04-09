349516928999d5d024f6ed64aeb0ed9ae9351a2f
victory <victorolade@gmail.com>
2023-04-09 03:13:45 +0000

    made last loop run in foreground

--- app/app.sh
+++ app/app.sh
@@ -2,5 +2,9 @@
 
 for i in {0..3}; do
   port=$((8080 + i))
-  exec websockify --web=/usr/share/novnc/ $port localhost:$((5900 + i)) &
+  if [ $i -eq 3 ]; then
+    websockify --web=/usr/share/novnc/ $port localhost:$((5900 + i))
+  else
+    websockify --web=/usr/share/novnc/ $port localhost:$((5900 + i)) &
+  fi
 done
