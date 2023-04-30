7890447056c8ced51eb4cdf8b8299bccb44431b4
victory <victorolade@gmail.com>
2023-04-29 23:17:46 -0400

    changed ip from local to cluster

--- app/app.sh
+++ app/app.sh
@@ -3,8 +3,8 @@
 for i in {0..3}; do
   port=$((8080 + i))
   if [ $i -eq 3 ]; then
-    websockify --web=/usr/share/novnc/ $port localhost:$((5900 + i))
+    websockify --web=/usr/share/novnc/ $port desktop.default.svc.cluster.local:$((5900 + i))
   else
-    websockify --web=/usr/share/novnc/ $port localhost:$((5900 + i)) &
+    websockify --web=/usr/share/novnc/ $port desktop.default.svc.cluster.local:$((5900 + i)) &
   fi
 done
